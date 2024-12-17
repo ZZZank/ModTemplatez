6401358d3f52862530b03c3522e4f29095b68d83
ZZZank <3410764033@qq.com>
2024-12-18 00:58:31 +0800

    interface injection example: make injected interfaces visible in sources

--- src/main/java/com/example/examplemod/inerface_injection/InjectionUsageExample.java
+++ src/main/java/com/example/examplemod/inerface_injection/InjectionUsageExample.java
@@ -1,18 +1,27 @@
 package com.example.examplemod.inerface_injection;
 
 import lombok.val;
+import net.minecraft.world.item.BoatItem;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author ZZZank
  */
 public class InjectionUsageExample {
 
-    public static void clearTag(ItemStack stack) {
-        if (stack == null) {
-            return;
-        }
+    /**
+     * use this syntax if `architectury.common.json` is not used
+     */
+    public static void clearTag(@NotNull ItemStack stack) {
         val inject = (InjectItemStack) (Object) stack;
         inject.exampleMod$clearTag();
+    }
+
+    /**
+     *
+     */
+    public static void injectedClearTag(@NotNull ItemStack stack) {
+        stack.exampleMod$clearTag();
     }
 }
